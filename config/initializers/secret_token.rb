746e5d67fcb66915e68718e4ac18936ffabe1a0d
Ubuntu <ubuntu@ip-172-31-5-243.ec2.internal>
2015-07-28 09:34:15 +0000

    Initiate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miamimarket::Application.config.secret_key_base = '830bd8baf3de2138b1f55d349801cf63e85009e3309eea6289f62047829d79d0bbe99d23cf22940a9d05b8f4f9cd754ad4973d8bce864eec6d71966c59e4bb14'
