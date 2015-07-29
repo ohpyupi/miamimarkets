a9a7400475a0873957c71c6bd69175eef5d0b03d
supergentle <ubuntu@ip-172-31-5-243.ec2.internal>
2015-07-29 07:54:00 +0000

    The carrierwave gem has been initialized

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
 	has_many	:comments
 	validates	:category, :inclusion => { :in => ["Rent/Housing", "Cars/Trucks", "Second hands", "Free topics"], :message => "Please choose a category over Rent/Housing, Cars/Trucks, Second hands, and Free topics."}
 	validates :title, :presence => { :message => "The titie must be necessary"}
+	mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728093450) do
+ActiveRecord::Schema.define(version: 20150729075028) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150728093450) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
